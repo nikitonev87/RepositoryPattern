deaed7ca039fb3d25906da86483d104186756b15
Nikolay Tonev <nikitonev87@gmail.com>
2017-09-21 11:57:06 +0100

    Create test project.

---
+++ source/RepositoryPattern.Test/UserRepositoryTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using RepositoryPattern.Data;
+using RepositoryPattern.Infrastructure;
+using RepositoryPattern.Models;
+using RepositoryPattern.Repositories;
+using System.Linq;
+
+namespace RepositoryPattern.Test
+{
+    [TestFixture]
+    public class UserRepositoryTest
+    {
+        [Test]
+        public void Add()
+        {
+            using (var connection = new SqliteConnection("Filename=:memory:"))
+            {
+                connection.Open();
+
+                var options = new DbContextOptionsBuilder<MyDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new MyDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new MyDbContext(options))
+                {
+                    var user = new User
+                    {
+                        FirstName = "Joe",
+                        LastName = "Bloggs"
+                    };
+
+                    var repository = new UserRepository(context);
+                    repository.Add(user);
+
+                    var unitOfWork = new UnitOfWork(context);
+                    unitOfWork.Save();
+                }
+
+                using (var context = new MyDbContext(options))
+                {
+                    var count = context.Users.Count();
+                    Assert.AreEqual(1, count);
+                }
+            }
+        }
+    }
+}
